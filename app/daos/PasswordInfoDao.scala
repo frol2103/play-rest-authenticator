f333dae37d23d2bf349290fe9068fcfd8b3ea1ba
Francois <github@frol.be>
2017-04-04 23:31:32 +0200

    refactor

--- app/daos/PasswordInfoDao.scala
+++ app/daos/PasswordInfoDao.scala
@@ -1,49 +1,15 @@
 package daos
 
-import scala.concurrent.Future
-
-import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.util.PasswordInfo
-import com.mohiva.play.silhouette.impl.daos.DelegableAuthInfoDAO
-
-import play.api.Play.current
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import models.User._
+import models.{Profile, User}
 import play.api.libs.json._
 
-import play.modules.reactivemongo.ReactiveMongoApi
-import play.modules.reactivemongo.json._
-import play.modules.reactivemongo.json.collection.JSONCollection
+class PasswordInfoDao extends AuthInfoDao[PasswordInfo] {
+  override def fromProfile(profile: Profile): Option[PasswordInfo] = profile.passwordInfo
 
-import models.User, User._
-
-class PasswordInfoDao extends DelegableAuthInfoDAO[PasswordInfo] with MongoPlayAppDao{
-  val users = reactiveMongoApi.db.collection[JSONCollection]("users")
-
-  def find(loginInfo:LoginInfo):Future[Option[PasswordInfo]] = for {
-    user <- users.find(Json.obj(
-      "profiles.loginInfo" -> loginInfo
-    )).one[User]
-  } yield user.flatMap(_.profiles.find(_.loginInfo == loginInfo)).flatMap(_.passwordInfo)
-
-  def add(loginInfo:LoginInfo, authInfo:PasswordInfo):Future[PasswordInfo] = 
-    users.update(Json.obj(
-      "profiles.loginInfo" -> loginInfo
-    ), Json.obj(
-      "$set" -> Json.obj("profiles.$.passwordInfo" -> authInfo)
-    )).map(_ => authInfo)
-
-  def update(loginInfo:LoginInfo, authInfo:PasswordInfo):Future[PasswordInfo] = 
-    add(loginInfo, authInfo)
-
-  def save(loginInfo:LoginInfo, authInfo:PasswordInfo):Future[PasswordInfo] = 
-    add(loginInfo, authInfo)
-
-  def remove(loginInfo:LoginInfo):Future[Unit] = 
-    users.update(Json.obj(
-      "profiles.loginInfo" -> loginInfo
-    ), Json.obj(
-      "$pull" -> Json.obj(
-        "profiles" -> Json.obj("loginInfo" -> loginInfo)
-      )
-    )).map(_ => ())
+  override val profileMongoAccessor: String = "passwordInfo"
+  override implicit val writes: Writes[PasswordInfo] = passwordInfoJsonFormat
 }
+
+
